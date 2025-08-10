baefe9972eac2f42228f8f217e9097ddeb2c1adb
Sowjanya <g.sowjanya@sap.com>
2025-08-10 13:47:46 +0530

    circle code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
-console.log("Hello, World!");
+const radius=[2,3,4,5]
 
-setTimeout(function(){
-   console.log("This is a delayed message after 5 seconds.");
-},5000)
+function calculateAreaOfCircle(radius){
+    let output=[];
+    for(let i=0;i<radius.length;i++){
+        output.push(Math.PI*radius[i]*radius[i]);
+       
+    }
+     return output;
+}
 
-console.log("OKKKs")
+console.log(calculateAreaOfCircle(radius))
+
+function calculateCircumferenceOfCircle(radius){
+    let output=[];
+    for(let i=0;i<radius.length;i++){
+        output.push(2*Math.PI*radius[i]);
+    }
+    return output;
+}
+
+console.log(calculateCircumferenceOfCircle(radius))
+
+function calculateDiameterOfCircle(radius){
+    let output=[];
+    for(let i=0;i<radius.length;i++){
+        output.push(2*radius[i]);
+    }
+    return output;
+}
+
+console.log(calculateDiameterOfCircle(radius))
