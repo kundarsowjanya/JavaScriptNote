7f26f835b8900ff924803d2278ada6abb9d4a8d7
Sowjanya <g.sowjanya@sap.com>
2025-08-13 10:47:38 +0530

    currying func added

--- js/curyy_func.js
+++ js/curyy_func.js
@@ -13,3 +13,16 @@ let multiply=function(x,y){
 
 let mutiPlyBYTwo= multiply.bind(this,2);;   // x is 2
 mutiPlyBYTwo(5); // Outputs: 10   //y is 5
+
+
+//uing closure to create a curried function
+
+let cuuriedMultiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplyByThree = cuuriedMultiply(3);
+multiplyByThree(4); // Outputs: 12   //y is 4
+
